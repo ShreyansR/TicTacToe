953b2f7579629d5cbd24feb948ff7896a7f01281
ShreyansR <shreyans.rishi@uoit.net>
2017-12-11 19:52:46 -0500

    Fixed a bug if game is draw

--- tictactoe.js
+++ tictactoe.js
@@ -112,6 +112,7 @@ function myFunction(){
       (document.getElementById('8').innerHTML != "I") &&
       (y == 0)){
         alert("DRAW!!");
+        reDraw();
         return false;
       }
 		}
